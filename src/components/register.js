e190b7ab0879f6650805225a559ec065c319e9a1
StevensMcFarlane <99173985+UltimateRootEJ@users.noreply.github.com>
2022-07-22 13:18:36 +0200

    Add files via upload

--- src/components/register.js
+++ src/components/register.js
@@ -2,26 +2,54 @@ import LoginHeader from './loginHeader'
 import NavBarL from './loginNavBar'
 import '../css/register.css'
 import { useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
+import { auth, db } from './firebase'
+import { addDoc } from 'firebase/firestore'
+import { collection } from 'firebase/firestore'
+
+
 
 export default function Register()
-{
-
-    const navigate=useNavigate();
-
+{const[email,setEmail]=useState('');
+const[password,setPassword]=useState('')
+const[name,setName]=useState('');
+const[surname,setSurname]=useState('')
+const navigate=useNavigate();
  
+const handleRegister=(()=>
+{
+const collectionRef=collection(db,"users");
+createUserWithEmailAndPassword(auth,email,password).then((userCredentials)=>{
+    const userr=userCredentials.user;
+    const user={
+        name:name,
+        surname:surname,
+        email:email,
+  
+    };
+    addDoc(collectionRef, user).then(()=>{
+        alert("added successfully")
+    }).catch((error)=>{console.log(error);alert("Error while adding")})
+})
 
-    const handleRegister = (() => {
-        navigate("/login")
-    })
+})
+
+
+
+
+        
+        // navigate("/login")
+    
     return(
         <div >
             <NavBarL />
                 <LoginHeader />
               <div className="registerDiv">
-                <input type="text" placeholder='Please enter your name'/>
-                <input type="text" placeholder='Please enter your surname'/>
-                <input type="email" placeholder='Please enter your email'/>
-                <input type="password" placeholder='Please enter your password'/>
+                <input type="text" placeholder='Please enter your name'onChange={(e)=>setName(e.target.value)}/>
+                <input type="text" placeholder='Please enter your surname'onChange={(e)=>setSurname(e.target.value)}/>
+                <input type="email" placeholder='Please enter your email' onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="password" placeholder='Please enter your password' onChange={(e)=>setPassword(e.target.value)}/>
                 <input type="password" placeholder='Re-type enter your password'/>
                 <button onClick={handleRegister} className='btnRegister'>Register</button>
               </div>
